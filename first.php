97af29377a1ec5f28af30caf3da781c2e0219f32
Yohannes Mesay <johnnymesay@gmail.com>
2024-04-26 05:39:54 +0300

    updated

--- first.php
+++ first.php
@@ -1,26 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Full Name: <input type="text" name="name" pattern="[a-zA-Z\s]+" required><span class="required">*</span><br> 
-    E-mail: <input type="email" name="email" required><span class="required">*</span><br>
-    Birthdate: <input type="date" name="birthdate" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" required><span class="required">*</span><br>
-    Biography:<br> <textarea name="biography" rows="5" cols="40"></textarea><br>
-    Gender:
-    <input type="radio" name="gender" value="female" required>Female
-    <input type="radio" name="gender" value="male" required>Male<span class="required">*</span><br>
-    Country: <input type="text" name="country" pattern="[a-zA-Z\s]*"><br>
-    <input type="submit" value="Submit">
-</form>
-<style>
-.required {
-    color: red;
-    position: relative;
-    left: 10px;
-    display: inline;
-    align-items: center;
-}
-</style>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST["name"]);
@@ -50,11 +30,12 @@ function validateFullName($name) {
 function validateEmail($email) {
     if (empty($email)) {
         return "E-mail is required.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    } elseif (preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email)) {
         return "E-mail must contain a valid email address.";
     }
     return ""; 
 }
+
 
 function validateBirthdate($birthdate) {
     if (empty($birthdate)) {
@@ -106,5 +87,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 }
 ?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Full Name: <input type="text" name="name" pattern="[a-zA-Z\s]+" ><span class="required">*</span><span> <?php echo $errorMsg; ?></span> <br> 
+    E-mail: <input type="email" name="email" required><span class="required">*</span><br>
+    Birthdate: <input type="date" name="birthdate" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" required><span class="required">*</span><br>
+    Biography:<br> <textarea name="biography" rows="5" cols="40"></textarea><br>
+    Gender:
+    <input type="radio" name="gender" value="female" required>Female
+    <input type="radio" name="gender" value="male" required>Male<span class="required">*</span><br>
+    Country: <input type="text" name="country" pattern="[a-zA-Z\s]*"><br>
+    <input type="submit" value="Submit">
+</form>
+<style>
+.required {
+    color: red;
+    position: relative;
+    left: 10px;
+    display: inline;
+    align-items: center;
+}
+</style>
+
 </body>
 </html>
